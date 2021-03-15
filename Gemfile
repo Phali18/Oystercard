b1f51e2b64031e1514bc2d4bb04f020118961e1a
Precieuse Hali <Precieusehali93x@hotmail.co.uk>
2021-03-15 16:20:33 +0000

    Exercise 6 updates

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 source 'https://rubygems.org'
+
+git_source(:github) {|Phali18| "https://github.com/Phali18/Oystercard#{Phali18}" }
 
 ruby '3.0.0'
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,16 @@
+require 'oystercard'
 
 describe Oystercard do
+  it "should check default balance is 0" do 
+    expect(subject.balance).to eq(0)
+  end 
+  
+  describe "#top_up" do
+    it "should increase balance by 10 if method called with value 10" do
+      expect{ subject.top_up 10 }.to change{ subject.balance }.by 10
+    end
+    it "should throw an expection if new balance would exceed 90" do
+      expect{ subject.top_up(91) }.to raise_error 
+    end
+  end
 end 
